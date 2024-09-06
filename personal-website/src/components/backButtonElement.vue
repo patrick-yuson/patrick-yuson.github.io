0d28acdc044d5be8de48b73ed2a8df4c373e8bff
Patrick Yuson <ntu7de@virginia.edu>
2024-09-06 13:01:30 -0400

    got project data to transfer based on the project clicked

---
+++ personal-website/src/components/backButtonElement.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useProjectEventsHandler } from '@/composables/projectEventsHandler';
+import iconElement from './iconElement.vue';
+
+const { currentProject } = useProjectEventsHandler()
+const handleClick = () => {
+    currentProject.value = null
+}
+</script>
+
+<template>
+    <div class="back-button-main">
+        <!-- <button class="button" @click="handleClick">Back</button> -->
+        <iconElement name="arrow_back_ios_new" :google="true" @click="handleClick" class="button"/>
+    </div>
+</template>
+
+<style>
+.back-button-main {
+    height: 50px;
+    width: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+}
+.back-button-main:hover {
+    cursor: pointer;
+    background-color: var(--hh-c-brown-lightest);
+}
+</style>
--- personal-website/src/components/projectElement.vue
+++ personal-website/src/components/projectElement.vue
@@ -1,11 +1,30 @@
 <script setup>
+import { computed } from 'vue'
+import { useProjectEventsHandler } from '@/composables/projectEventsHandler';
+import backButtonElement from './backButtonElement.vue';
+import songData from '../assets/songData.json'
+
+const { currentProject } = useProjectEventsHandler()
+const currProjectData = computed(() => {
+    return songData.projects.find(project => project.id === currentProject.value)
+})
 </script>
 
 <template>
     <div class="project-main">
-        <div>Hello World</div>
+        <div class="project-back-button">
+            <backButtonElement />
+        </div>
+        <div class="project-header">{{ currProjectData.name }}</div>
     </div>
 </template>
 
 <style>
+.project-main {
+    margin: var(--padding-component);
+}
+.project-header {
+    font-size: 60px;
+    font-weight: bold;
+}
 </style>
